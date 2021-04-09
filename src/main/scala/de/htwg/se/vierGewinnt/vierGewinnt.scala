c7e36a6f2abb39c44d4ab8cf4a4e7f4e195cc898
woot38 <christian.holzinger@htwg-konstanz.de>
2021-04-09 13:06:23 +0200

    path fix

--- src/main/scala/de/htwg/se/vierGewinnt/vierGewinnt.scala
+++ src/main/scala/de/htwg/se/vierGewinnt/vierGewinnt.scala
@@ -5,8 +5,6 @@ import de.htwg.se.vierGewinnt.model.Player
 object vierGewinnt {
   def main(args: Array[String]): Unit = {
     val Player1 = Player("Tom")
-    printf("Hello Player1 : "+ Player1)
+    printf("Hello Player1 : " + Player1)
   }
 }
-
-
